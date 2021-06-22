c59f7a4c3f31ebb830582cf090617861b7766efe
niedzwiadek7 <damiankliber@gmail.com>
2021-06-22 13:38:57 +0200

    Resolve id user in login

--- server/controllers/Users/loginUser.js
+++ server/controllers/Users/loginUser.js
@@ -2,5 +2,6 @@ const jwt = require('jsonwebtoken')
 
 exports.login = async (req, res) => {
     const token = jwt.sign({ id: req.body._id }, process.env.JWT_TOKEN)
-    res.send({ token })
+    const user = req.body._id
+    res.send({ token, user })
 }
